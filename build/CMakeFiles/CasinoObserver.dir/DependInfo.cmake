f7443ef719b443c6337a2d5603a99350b0ef3377
Kirill Baidakov <kabaidakov@edu.hse.ru>
2024-12-13 15:58:00 +0300

    smth

---
+++ build/CMakeFiles/CasinoObserver.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/baidakoukiryl/CasinoObserver/game_loop.cpp" "CMakeFiles/CasinoObserver.dir/game_loop.cpp.o" "gcc" "CMakeFiles/CasinoObserver.dir/game_loop.cpp.o.d"
+  "/Users/baidakoukiryl/CasinoObserver/state_manager.cpp" "CMakeFiles/CasinoObserver.dir/state_manager.cpp.o" "gcc" "CMakeFiles/CasinoObserver.dir/state_manager.cpp.o.d"
+  "/Users/baidakoukiryl/CasinoObserver/textures_loader.cpp" "CMakeFiles/CasinoObserver.dir/textures_loader.cpp.o" "gcc" "CMakeFiles/CasinoObserver.dir/textures_loader.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CasinoObserver.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CasinoObserver.dir/game_loop.cpp.o"
+  "CMakeFiles/CasinoObserver.dir/game_loop.cpp.o.d"
+  "CMakeFiles/CasinoObserver.dir/state_manager.cpp.o"
+  "CMakeFiles/CasinoObserver.dir/state_manager.cpp.o.d"
+  "CMakeFiles/CasinoObserver.dir/textures_loader.cpp.o"
+  "CMakeFiles/CasinoObserver.dir/textures_loader.cpp.o.d"
+  "CasinoObserver"
+  "CasinoObserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CasinoObserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

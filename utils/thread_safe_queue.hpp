8301cffe73bb2291703cd57a9e4111871d518a3b
Kirill Baidakov <kabaidakov@edu.hse.ru>
2025-01-10 16:29:53 +0300

    base thread safe queue

---
+++ utils/thread_safe_queue.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <mutex>
+#include <queue>
+
+// Thread safe queue
+// It is slow and basic implementation
+// TODO: lock-free queue
+template <typename T>
+class TSQueue {
+public:
+    TSQueue() = default;
+
+    TSQueue(const TSQueue& other_tsq) = delete;
+    TSQueue& operator=(const TSQueue& other_tsq) = delete;
+
+    TSQueue(TSQueue&& other_tsq) = delete;
+    TSQueue&& operator=(TSQueue& other_tsq) = delete;
+
+    ~TSQueue() = default;
+
+    std::optional<T> pop() {
+        std::lock_guard<std::mutex> lg{mtx_};
+        if (data_.empty()) {
+            return std::nullopt;
+        }
+
+        T elem = data_.front();
+        data_.pop();
+        return elem;
+    }
+
+    void push(const T& value) {
+        std::lock_guard<std::mutex> lg{mtx_};
+        data_.push(value);
+    }
+
+    size_t size() const {
+        std::lock_guard<std::mutex> lg{mtx_};
+        return data_.size();
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> lg{mtx_};
+        return data_.empty();
+    }
+
+private:
+    std::queue<T> data_;
+    std::mutex mtx_;
+};

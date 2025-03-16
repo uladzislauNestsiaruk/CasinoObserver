3ceeac44230f836d1928207948ac4cd92fa8d144
Kirill Baidakov <kabaidakov@edu.hse.ru>
2025-03-16 18:32:14 +0300

    merged work version

--- game_states/table_state.cpp
+++ game_states/table_state.cpp
@@ -1,31 +1,30 @@
 #include "table_state.hpp"
 
 TableState::TableState(const std::string& objects_path, StateManager* manager,
-               table_creater_t table_creater)
-        : AbstractGameState(objects_path), stop_game_thread_(false), run_game_(false),
-          game_exec_thr_(([this] { GameExecutor(); })) {
-        table_ = table_creater(logs_, render_events_manager_.GetRenderQueue());
-        std::shared_ptr<GameObject> root_game_object_ = objects_manager_.FindObjectByTag("root");
-        root_game_object_->Resize(manager->GetWindowSize());
-        for (const auto& place : kGamblersPlaces) {
-            for (const auto& person_tag : place) {
-                root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
-                                              CommonGOEventHandlers::PlayerHandler, person_tag);
-            }
+                       table_creater_t table_creater)
+    : AbstractGameState(objects_path), stop_game_thread_(false), run_game_(false),
+      game_exec_thr_(([this] { GameExecutor(); })) {
+    table_ = table_creater(logs_, render_events_manager_.GetRenderQueue());
+    std::shared_ptr<GameObject> root_game_object_ = objects_manager_.FindObjectByTag("root");
+    root_game_object_->Resize(manager->GetWindowSize());
+    for (const auto& place : kGamblersPlaces) {
+        for (const auto& person_tag : place) {
+            root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
+                                          CommonGOEventHandlers::PlayerHandler, person_tag);
         }
-
-        root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
-                                      CommonGOEventHandlers::ReturnButtonHandler, "return_button");
-        root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
-                                      CommonGOEventHandlers::SelectButtonHandler, "select_button");
-        root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
-                                      CommonGOEventHandlers::DealButtonHandler, "deal_button");
-        root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
-                                      CommonGOEventHandlers::BanButtonHandler, "ban_button");
-
-        render_events_manager_.AddHandler("change_phase",
-                                          CommonREMEventHandlers::ChangePhaseHandler);
     }
+
+    root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
+                                  CommonGOEventHandlers::ReturnButtonHandler, "return_button");
+    root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
+                                  CommonGOEventHandlers::SelectButtonHandler, "select_button");
+    root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
+                                  CommonGOEventHandlers::DealButtonHandler, "deal_button");
+    root_game_object_->AddHandler(sf::Event::MouseButtonPressed,
+                                  CommonGOEventHandlers::BanButtonHandler, "ban_button");
+
+    render_events_manager_.AddHandler("change_phase", CommonREMEventHandlers::ChangePhaseHandler);
+}
 
 void TableState::HandleEvent(const sf::Event& event) {
     if (event.type != sf::Event::MouseButtonPressed) {
@@ -61,9 +60,9 @@ void TableState::Draw(StateManager* manager) {
 void TableState::GameExecutor() {
     while (!stop_game_thread_) {
         if (run_game_.load()) {
-            selected_players_.clear();
-            table_->GameIteration();
+            table_->ClearRollback();
             run_game_.store(false);
+            table_->GameIteration();
         }
     }
 }
@@ -75,12 +74,30 @@ TableState::~TableState() {
 
 void TableState::BanPlayers() {
     table_->SetWasActionPerformed(true);
-    while (table_->GetWasActionPerformed()) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    while (!table_->IsGameFinished()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    table_->RollbackGame();
+    render_events_manager_.Clear();
+    while (!logs_.empty()) {
+        logs_.pop();
     }
 
     for (const auto& person_tag : selected_players_) {
         table_->RemovePlayer(person_tag);
     }
+
     selected_players_.clear();
+    table_->SetWasActionPerformed(false);
+    for (const std::string& card_id : {"first", "second", "third", "fourth", "fivth"}) {
+        json render_event;
+        render_event["event"]["type"] = "change_phase";
+        render_event["new_phase"] = "empty";
+        render_event["tag"] = card_id + "_central_card";
+        render_event["delay"] = 0;
+
+        objects_manager_.FindObjectByTag(card_id + "_central_card")->FinishPhase();
+        table_->AddRenderEvent(render_event);
+    }
 }
